d8599c91fb162779fe33f2a349048b6b7c4919e5
jakeeheller <40074555-jakeeheller@users.noreply.replit.com>
2025-02-28 01:57:14 +0000

    Add logo and adjust primary theme color
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/f37648ea-e082-4841-abfc-428660752a4d/f7e2d950-2277-4a66-94ec-f8fff3fddb68.jpg

--- client/src/components/layout/nav.tsx
+++ client/src/components/layout/nav.tsx
@@ -1,11 +1,12 @@
 import { cn } from "@/lib/utils";
+import Logo from "./logo";
 
 export default function Nav() {
   return (
     <nav className="border-b">
       <div className="container mx-auto px-4">
         <div className="flex h-16 items-center">
-          <h1 className="text-xl font-bold">Body Note</h1>
+          <Logo />
         </div>
       </div>
     </nav>
